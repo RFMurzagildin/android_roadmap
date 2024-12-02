96035092d9c7afc50812037149f8e4f55db8a122
Ranis Murzagildin <murza.ran@mail.ru>
2024-12-03 01:17:20 +0300

    added navigation_component

---
+++ NavigationComponent/app/src/main/java/com/app/navigationcomponent/SettingsFragment.kt
@@ -0,0 +1,15 @@
+package com.app.bottomnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.app.navigationcomponent.R
+
+
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
